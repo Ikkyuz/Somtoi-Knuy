fd2ccd19c677b34f10acc0687e9e6411ac8e9325
Ikkyuz <djdjv999@gmail.com>
2026-02-23 15:56:07 +0700

    :white_check_mark: Add authorization swagger

--- backend/swagger.js
+++ backend/swagger.js
@@ -6,7 +6,21 @@ const doc = {
     description: 'API สำหรับจัดการสมาชิก'
   },
   host: 'localhost:3000',
-  schemes: ['http']
+  schemes: ['http'],
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      in: 'header',       // ส่งผ่าน Header
+      name: 'Authorization', // ชื่อ Header
+      description: 'ใส่ Token ในรูปแบบ: Bearer <your_token>'
+    }
+  },
+  // 2. ใช้ Security กับทุก Endpoint (Optional: ถ้าอยากให้ล็อกทั้งระบบ)
+  security: [
+    {
+      bearerAuth: []
+    }
+  ]
 };
 
 const outputFile = './swagger-output.json';
